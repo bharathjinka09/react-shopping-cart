0e1849647645c4110d831e09164aad424f884236
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-01 22:14:17 +0530

    Updates

---
+++ precache-manifest.5bd18778bd8a17c65c13fec1e7662086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c7af4a9d78e1185ad32bba7091065e",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "95c1745f4e535a064c9c",
+    "url": "/react-shopping-cart/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "2d81aa7b8128baaa69c4",
+    "url": "/react-shopping-cart/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "95c1745f4e535a064c9c",
+    "url": "/react-shopping-cart/static/js/2.2652018f.chunk.js"
+  },
+  {
+    "revision": "2d81aa7b8128baaa69c4",
+    "url": "/react-shopping-cart/static/js/main.a6e2e69d.chunk.js"
+  },
+  {
+    "revision": "89264515d9191e288e96",
+    "url": "/react-shopping-cart/static/js/runtime~main.b0014d54.js"
+  }
+]);
